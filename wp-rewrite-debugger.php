0d12b90f6b0e98b64f881efa30501a76088458db
joerivanveen <nonstockphoto@gmail.com>
2023-04-19 16:54:01 +0200

    feat: flush rewrite rules when the custom slug has gone

--- wp-rewrite-debugger.php
+++ wp-rewrite-debugger.php
@@ -6,24 +6,28 @@ Plugin URI: https:///joerivanveen.com
 Description: Debug rewrite issues by logging before and after situations
 Author: Joeri van Veen
 Author URI: https://joerivanveen.com
-Version: 0.0.1
+Version: 0.0.2
 */
 defined( 'ABSPATH' ) || die();
 // This is plugin nr. 13 by Ruige hond. It identifies as: ruigehond013.
-define( 'RUIGEHOND013_VERSION', '0.0.1' );
+define( 'RUIGEHOND013_VERSION', '0.0.2' );
 // Startup the plugin
 add_action( 'parse_request', 'ruigehond013' );
 //
 function ruigehond013() {
-	if ( 0 === strpos( ( $uri = $_SERVER['REQUEST_URI'] ), '/portfolio/' ) ) {
+	if ( 0 === strpos( ( $uri = $_SERVER['REQUEST_URI'] ), '/project/' )) {
 		register_shutdown_function( static function () use ( $uri ) {
 			global $wp_query;
 			$file = __DIR__ . '/' . date( 'Y-m-d' ) . '-wp-rewrite-debugger.log';
 			// find out if a 404 was sent, because we need to log stuff then
 			if ( $wp_query->is_404() || ! file_exists($file) ) {
 				ruigehond013_log( $uri, $file);
-				ruigehond013_log( var_export( get_option( 'rewrite_rules' ), true ), $file);
+				ruigehond013_log( var_export( ($rules = get_option( 'rewrite_rules' )), true ), $file);
 				ruigehond013_log( var_export( $wp_query, true ), $file );
+				if ($wp_query->is_404() && false === isset($rules['project/[^/]+/attachment/([^/]+)/?$'])) {
+					flush_rewrite_rules();
+					ruigehond013_log('REWRITE RULES WERE FLUSHED', $file);
+				}
 			}
 		} );
 	}
